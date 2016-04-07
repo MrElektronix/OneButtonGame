6e78df5d47a62c229aaaa8e4799ea4f7a0695385
MrElektronix <milovanpelt@gmail.com>
2016-04-07 18:25:38 +0100

    One Button 2.0 (Donderdag 7 April)

--- OneButton.js
+++ OneButton.js
@@ -1,16 +1,33 @@
 window.addEventListener("load", function() {
+	
+	keyState = {};
+	window.addEventListener('keydown',function(e){
+		keyState[e.keyCode || e.which] = true;
+	},true);
+	window.addEventListener('keyup',function(e){
+		keyState[e.keyCode || e.which] = false;
+	},true);
+
 	canvas = document.getElementById("canvas");
 	ctx = canvas.getContext("2d");
-
-	canvas.width = 800;
-	canvas.height = 800;
+	canvas.style.visibility = "visible";
+	canvas.width = window.innerWidth;
+	canvas.height = window.innerHeight;
 
 	Initialize();
 
-	ctx.font = "40px Arial";
 	var StartText = "Press L to begin (and to play with)";
-	ctx.fillText(StartText,80,400);
+	PositionText(StartText);
 
+
+});
+
+var Player = {
+	width: 100,
+	height: 100
+}
+function BeginGame() {
+	canvas.style.visibility = "visible";
 	setInterval(function(){
 		if (Timer == 0){
 			Timer = 0;
@@ -20,18 +37,29 @@ window.addEventListener("load", function() {
 			Timer--;
 		}
 	}, 1000);
-});
-
-var Player = {
-	width: 100,
-	height: 100
-}
-function BeginGame() {
+	gameMusic.volume = 1;
+	if (typeof gameMusic.loop == 'boolean'){
+    	gameMusic.loop = true;
+	}else {
+    gameMusic.addEventListener('ended', function() {
+        this.currentTime = 0;
+        this.play();
+    }, false);
+	}
 	gameMusic.play();
-	Drawing = setInterval(function(){draw()}, 20);
+	Drawing = setInterval(function(){draw()}, DrawingIntervalTime);
 	Shrinking = setInterval(function(){ChangeObject()}, shrinkIntervalTime);
-
 }
+
+function PositionText(Text) {
+	TextWidth = Math.round(ctx.measureText(Text).width);
+	TextHeight = 40;
+
+	TextX = ((canvas.width / 2) - (TextWidth / 2));
+	TextY = ((canvas.height / 2) - (TextHeight / 2));
+
+	ctx.fillText(Text,TextX,TextY);
+}	
 
 function EndGame() {
 	clearInterval(Drawing);
@@ -41,19 +69,26 @@ function EndGame() {
 	ctx.fillText(EndText,250,400);
 
 	var Endscore = "Je eind score is: " + scoreNumber + " punten";
-	ctx.fillText(Endscore,100,450);
+	ctx.fillText(Endscore,250,450);
 
 }
 
 function Initialize() {
+	keyLoop();
+	allowChange = true;
+	expanding = false;
 	Game = false;
-	ExpandSpeed = 10;
+	ExpandSpeed = 3;
 	ShrinkSpeed = 1.3;
-	moveNumber = 200;
 	scoreNumber = 0;
-	Timer = 20;
+	Timer = 60;
 	shrinkIntervalTime = 50;
-	gameMusic = new Audio('kika.mp3');
+	DrawingIntervalTime = 11;
+	moveNumber = (DrawingIntervalTime * 50);
+
+	ctx.font = "40px Arial";
+	ctx.fillStyle = 'white';
+	gameMusic = new Audio('game-music-2.wav');
 
 	ColorList1 = ["red", "green"];
 	GoalColor = Math.floor(Math.random()*ColorList1.length);
@@ -67,7 +102,6 @@ function Initialize() {
 	GoalWidthSmall = [50, 100];
 	GoalWHS = Math.floor(Math.random()*GoalWidthSmall.length);
 
-
 	GoalWidth = 200;
 	GoalHeight = 200;
 		
@@ -75,18 +109,18 @@ function Initialize() {
 	PlayerY = 600;
 	GoalX = 50;
 	GoalY = 80;
-	IndicatorX = (PlayerX - 5);
-	IndicatorY = (PlayerY - 5);
-	IndicatorWidth = (Player.width + 10);
-	IndicatorHeight = (Player.height + 10);
+	//IndicatorX = (PlayerX - 5);
+	//IndicatorY = (PlayerY - 5);
+	//IndicatorWidth = (Player.width + 10);
+	//IndicatorHeight = (Player.height + 10);
 
 	direction = -1;
 	distanceX = PlayerX - GoalX;
 	distanceY = PlayerY - GoalY;
-	r = 0;
-	g = 255;
-	b = 0;
-	alpha = 0.2;
+	//r = 0;
+	//g = 255;
+	//b = 0;
+	//alpha = 0.2;
 
 }
 
@@ -96,9 +130,11 @@ function MakePlayer() {
 }
 
 function MakeGoal(GoalX, GoalY, GoalWidth, GoalHeight) {
-	ctx.lineWidth = 1;
-	ctx.strokeStyle = (ColorList1[GoalColor]);
-	ctx.strokeRect(GoalX, GoalY, GoalWidth, GoalHeight);
+	//ctx.lineWidth = 1;
+	//ctx.strokeStyle = (ColorList1[GoalColor]);
+	//ctx.strokeRect(GoalX, GoalY, GoalWidth, GoalHeight);
+	ctx.fillStyle = 'white';
+	ctx.fillRect(GoalX, GoalY, GoalWidth, GoalHeight);
 }
 
 function Indicator() {
@@ -116,7 +152,7 @@ function Indicator() {
 function Attributes() {
 
 	var Score = "Score: " + scoreNumber;
-	ctx.fillText(Score,550,50);
+	ctx.fillText(Score,1500,50);
 
 	var Time = "Time: " + Timer;
 	ctx.fillText(Time,10,50);
@@ -143,47 +179,56 @@ function ChangeObject() {
 
 }
 
+function draw() {
+	ctx.clearRect(0, 0, canvas.width, canvas.height);
+	MakeGoal(GoalX, GoalY, GoalWidth, GoalHeight);
+	Attributes();
+	MakePlayer();
+	update();
+	
+}
+
 function update() {
-	scoreNumber += Math.floor(Math.random()*20);
-	littleX = Math.floor((Math.random()*((canvas.width / 2))) + 300);
-	littleY = 500;
-
-	bigX = Math.floor((Math.random()*((canvas.width / 2) - 400)) + 50);
-	bigY = 100;
-
-	//speler is rechts onder het goal en beweest naar links en boven
-	if(direction == -1 && PlayerX > GoalX && PlayerY > GoalY)
-	{
-		expanding = true;
-		movePlayer((distanceX / moveNumber),(distanceY / moveNumber));
-	}else if(direction == -1)
-	{
-		//goal geraakt
-		//bonusPoint();
+	//scoreNumber += Math.floor(Math.random()*20);
+    if (allowChange == true && direction == -1 && PlayerX >= GoalX - 0.1 && PlayerX <= GoalX + 0.1 && PlayerY >= GoalY - 0.1 && PlayerY <= GoalY + 0.1) {
+    	bonusPoint();
+    	allowChange = false;
 		setTimeout(function(){
+		allowChange = true;
+		littleX = Math.floor((Math.random()*((canvas.width / 2))) + 300);
+		littleY = 300;
 		direction = 1;
 		changeGoal(littleX,littleY,GoalWidthSmall[GoalWHS],GoalWidthSmall[GoalWHS]);
-		distanceX = Math.floor(GoalX - PlayerX);
-		distanceY = Math.floor(GoalY - PlayerY);
-		}, 100);
+		distanceX = GoalX - PlayerX;
+		distanceY = GoalY - PlayerY;
+		}, 0);
+	}
+	else if (direction == -1) {
+        expanding = true;
+		movePlayer((distanceX / moveNumber),(distanceY / moveNumber));
+		
+		//IndicatorX = (PlayerX - 5);
+		//IndicatorY = (PlayerY - 5);
+		//Indicator();
 	}
 
-
-	if(direction == 1 && PlayerX < GoalX && PlayerY < GoalY)
-	{
-		expanding = false;
-		movePlayer((distanceX / -moveNumber),(distanceY / -moveNumber));
-	}else if(direction == 1)
-	{
-		//bonusPoint();
+	if (allowChange == true && direction == 1 && PlayerX >= GoalX - 0.1 && PlayerX <= GoalX + 0.1 && PlayerY >= GoalY - 0.1 && PlayerY <= GoalY + 0.1) {
+		bonusPoint();
+		allowChange = false;
 		setTimeout(function(){
+		allowChange = true;
+		bigX = Math.floor((Math.random()*((canvas.width / 2) - 400)) + 50);
+		bigY = 100;
 		direction = -1;
 		changeGoal(bigX,bigY,GoalWidthBig[GoalWHB],GoalWidthBig[GoalWHB]);
-		distanceX = Math.floor(PlayerX - GoalX);
-		distanceY = Math.floor(PlayerY - GoalY);
-		}, 100);
+		distanceX = PlayerX - GoalX;
+		distanceY = PlayerY - GoalY;
+		}, 0);
 	}
-
+	else if (direction == 1){
+		expanding = false;
+		movePlayer((distanceX / -moveNumber),(distanceY / -moveNumber));
+	}
 
 
 }
@@ -193,7 +238,7 @@ function update() {
 function changeGoal(newX, newY, width, height)
 {
 	//verander pos en size van goal
-
+	console.log("Change Log");
 	GoalX = newX;
 	GoalY = newY;
 	GoalWidth = width;
@@ -204,50 +249,25 @@ function movePlayer(mx,my)
 	PlayerX -= mx;
 	PlayerY -= my;
 
-
-}
-
-function draw() {
-	ctx.clearRect(0, 0, canvas.width, canvas.height);
-	MakeGoal(GoalX, GoalY, GoalWidth, GoalHeight);
-	Attributes();
-	MakePlayer();
-	update();
-	
-	/*
-	if (PlayerX >= GoalX - 0.2 && PlayerX <= GoalX + 0.2 && PlayerY >= GoalY - 0.2 && PlayerY <= GoalY + 0.2) {
-		
-	}
-	if (PlayerX <= GoalX - 0.2 && PlayerX >= GoalX + 0.2 && PlayerY <= GoalY - 0.2 && PlayerY >= GoalY + 0.2) {
-	}
-	else {
-
-		*/
-		
-		//IndicatorX = (PlayerX - 5);
-		//IndicatorY = (PlayerY - 5);
-
-		//Indicator();
-	//}
-	//
 }
 
 function bonusPoint() {
-	pointcondition1 = (Player.width > (GoalWidthBig[GoalWHB] - 10) && Player.width < GoalWidthBig[GoalWHB] && Player.height > (GoalWidthBig[GoalWHB] - 10) && Player.height < GoalWidthBig[GoalWHB]);
-	pointcondition2 = (Player.width > (GoalWidthBig[GoalWHB] - 20) && Player.width < GoalWidthBig[GoalWHB] && Player.height > (GoalWidthBig[GoalWHB] - 20) && Player.height < GoalWidthBig[GoalWHB]);
-	pointcondition3 = (Player.width > (GoalWidthBig[GoalWHB] - 30) && Player.width < GoalWidthBig[GoalWHB] && Player.height > (GoalWidthBig[GoalWHB] - 30) && Player.height < GoalWidthBig[GoalWHB]);
-	pointcondition4 = (Player.width > (GoalWidthBig[GoalWHB] - 40) && Player.width < GoalWidthBig[GoalWHB] && Player.height > (GoalWidthBig[GoalWHB] - 40) && Player.height < GoalWidthBig[GoalWHB]);
-	pointcondition5 = (Player.width > (GoalWidthBig[GoalWHB] - 50) && Player.width < GoalWidthBig[GoalWHB] && Player.height > (GoalWidthBig[GoalWHB] - 50) && Player.height < GoalWidthBig[GoalWHB]);
-	pointcondition6 = (Player.width > (GoalWidthBig[GoalWHB] - 60) && Player.width < GoalWidthBig[GoalWHB] && Player.height > (GoalWidthBig[GoalWHB] - 60) && Player.height < GoalWidthBig[GoalWHB]);
-	pointcondition7 = (Player.width > (GoalWidthBig[GoalWHB] - 70) && Player.width < GoalWidthBig[GoalWHB] && Player.height > (GoalWidthBig[GoalWHB] - 70) && Player.height < GoalWidthBig[GoalWHB]);
+	pointcondition1 = (Player.width == GoalWidth && Player.height == GoalHeight);
+	pointcondition2 = (Player.width > (GoalWidth - 10) && Player.width < GoalWidth && Player.height > (GoalHeight - 10) && Player.height < GoalHeight);
+	pointcondition3 = (Player.width > (GoalWidth - 20) && Player.width < GoalWidth && Player.height > (GoalHeight - 20) && Player.height < GoalHeight);
+	pointcondition4 = (Player.width > (GoalWidth - 30) && Player.width < GoalWidth && Player.height > (GoalHeight - 30) && Player.height < GoalHeight);
+	pointcondition5 = (Player.width > (GoalWidth - 40) && Player.width < GoalWidth && Player.height > (GoalHeight - 40) && Player.height < GoalHeight);
+	pointcondition6 = (Player.width > (GoalWidth - 50) && Player.width < GoalWidth && Player.height > (GoalHeight - 50) && Player.height < GoalHeight);
+	pointcondition7 = (Player.width > (GoalWidth - 60) && Player.width < GoalWidth && Player.height > (GoalHeight - 60) && Player.height < GoalHeight);
 
-	pointvalue1 = 900;
-	pointvalue2 = 800;
-	pointvalue3 = 700;
-	pointvalue4 = 600;
-	pointvalue5 = 500;
-	pointvalue6 = 400;
+	pointvalue1 = 1000;
+	pointvalue2 = 900;
+	pointvalue3 = 800;
+	pointvalue4 = 700;
+	pointvalue5 = 600;
+	pointvalue6 = 500;
 	pointvalue7 = 400;
+	pointvalue8 = 350;
 
 	BonusPointTime = 100;
 
@@ -260,17 +280,18 @@ function bonusPoint() {
 		ctx.fillText("+ " + pointvalue1,670,90);
 
 	}
-	else if (pointcondition2) {
+	else if(pointcondition2) {
 		setTimeout(function(){
 		scoreNumber = scoreNumber += pointvalue2;
 		Attributes();
 		}, BonusPointTime);
 		console.log(pointvalue2);
 		ctx.fillText("+ " + pointvalue2,670,90);
+
 	}
 	else if (pointcondition3) {
 		setTimeout(function(){
-		scoreNumber += pointvalue3;
+		scoreNumber = scoreNumber += pointvalue3;
 		Attributes();
 		}, BonusPointTime);
 		console.log(pointvalue3);
@@ -308,35 +329,41 @@ function bonusPoint() {
 		console.log(pointvalue7);
 		ctx.fillText("+ " + pointvalue7,670,90);
 	}
+	else {
+		setTimeout(function(){
+		scoreNumber += pointvalue8;
+		Attributes();
+		}, BonusPointTime);
+		console.log(pointvalue8);
+		ctx.fillText("+ " + pointvalue8,670,90);
+	}
 }
 
-document.onkeyup = function(evt) {
+function keyLoop() {
 	var L = 76;
-	if (evt.keyCode == L && Game == false) {
+
+	if (keyState[L] && Game == false) {
 		BeginGame();
 		Game = true;
 	}
-	if (evt.keyCode == L && expanding == true && Game == true) {
+	if (keyState[L] && expanding == true && Game == true) {
 		Player.width += ExpandSpeed;
 		Player.height += ExpandSpeed;
-		IndicatorWidth += ExpandSpeed;
-		IndicatorHeight += ExpandSpeed;
+		//IndicatorWidth += ExpandSpeed;
+		//IndicatorHeight += ExpandSpeed;
 	}
-	if (evt.keyCode == L && expanding == false && Game == true) {
+	if (keyState[L] && expanding == false && Game == true) {
 		Player.width -= ExpandSpeed;
 		Player.height -= ExpandSpeed;
-		IndicatorWidth -= ExpandSpeed;
-		IndicatorHeight -= ExpandSpeed;
+		//IndicatorWidth += ExpandSpeed;
+		//IndicatorHeight += ExpandSpeed;
 	}
 
-
-	/*
-	if (evt.keyCode == L && win == true) {
-		Won();
-	}
-	*/
+	setTimeout(keyLoop, 40);
 }
 /*
+if (PlayerX <= GoalX - 0.2 && PlayerX >= GoalX + 0.2 && PlayerY <= GoalY - 0.2 && PlayerY >= GoalY + 0.2) {
+	}
 if (Player.width == GoalWHB[GoalWidthBig] && Player.height == GoalWHB[GoalWidthBig]) {
 		win = true;
 		setTimeout(function(){ 
